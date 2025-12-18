f0aa2b0004e4705f2a764f6b2cb7cc8e31702caa
Poonam Vishwakarma <agclub31@gmail.com>
2025-12-18 22:49:22 +0530

    Fix routing setup by enabling API routes in app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import express, { json } from 'express';
+import router from './routes/auth.routes.js'
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app= express();
 const Port= 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use("/api/v1",router);
+app.use("/api/v1",router);
 
 app.listen(Port,()=>{
     console.log(`Server is running on localhost:5000`)
