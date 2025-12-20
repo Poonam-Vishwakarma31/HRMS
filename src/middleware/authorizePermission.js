f58a6aeb2a89e12e41bf8c56f44f889ef1496804
Poonam Vishwakarma <agclub31@gmail.com>
2025-12-20 21:26:39 +0530

    replaced role bsed access to permission based access

---
+++ src/middleware/authorizePermission.js
@@ -0,0 +1,22 @@
+
+export const authorizePermissions = (...requiredPermissions) => {
+  return (req, res, next) => {
+    // Safety check
+    if (!req.user || !Array.isArray(req.user.permissions)) {
+      return res.status(403).json({ message: "Access denied" });
+    }
+
+    // Check all required permissions
+    const hasAllPermissions = requiredPermissions.every(permission =>
+      req.user.permissions.includes(permission)
+    );
+
+    if (!hasAllPermissions) {
+      return res.status(403).json({ message: "Forbidden" });
+    }
+
+    next();
+  };
+};
+
+
