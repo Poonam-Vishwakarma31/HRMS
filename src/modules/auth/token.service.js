c6664bbb778d42959451d3e7fcbf9fdde9883124
Poonam Vishwakarma <agclub31@gmail.com>
2025-12-21 18:35:45 +0530

    correction of permission to permissions

--- src/modules/auth/token.service.js
+++ src/modules/auth/token.service.js
@@ -3,9 +3,9 @@ import { ROLE_PERMISSIONS } from "../../config/rolePermission.js";
 
 export const createToken= (id, email, role)=>{
 
-    const permission= ROLE_PERMISSIONS[role] || [];
+    const permissions= ROLE_PERMISSIONS[role] || [];
 
 
-    const token=jwt.sign({id, email, role, permission},process.env.JWT_TOKEN,{expiresIn: "1d"})
+    const token=jwt.sign({id, email, role, permissions},process.env.JWT_TOKEN,{expiresIn: "1d"})
     return token;
 }
