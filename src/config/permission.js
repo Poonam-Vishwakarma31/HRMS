ff34925f8e3c2c61dd8b7d6df728b4605bf41ba7
Poonam Vishwakarma <agclub31@gmail.com>
2025-12-21 20:11:45 +0530

    Modified some data for leave permission

--- src/config/permission.js
+++ src/config/permission.js
@@ -7,18 +7,14 @@ export const PERMISSIONS = {
   EMPLOYEE_UPDATE_SELF: "employee:update:self",
   EMPLOYEE_UPDATE_ALL: "employee:update:all",
 
-  //SALARY / PAYROLL 
-  SALARY_VIEW_SELF: "salary:view:self",
-  SALARY_VIEW_ALL: "salary:view:all",
-  PAYROLL_VIEW: "payroll:view",
-  PAYROLL_GENERATE: "payroll:generate",
-
+ 
   // LEAVE REQUESTS 
   LEAVE_CREATE: "leave:create",
   LEAVE_VIEW_SELF: "leave:view:self",
   LEAVE_VIEW_ALL: "leave:view:all",
   LEAVE_APPROVE: "leave:approve",
   LEAVE_REJECT: "leave:reject",
+  LEAVE_CANCLE: "leave:cancle",
 
   //SYSTEM / ADMIN 
   ROLE_ASSIGN: "role:assign",
