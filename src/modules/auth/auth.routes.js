a8d64d8e0a6a7851bf36941a556d52cd84af2005
Poonam Vishwakarma <agclub31@gmail.com>
2025-12-22 23:24:11 +0530

    modified route for create user and also addd a route for manager assigning to employee

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -1,10 +1,34 @@
 import { Router } from "express";
-import * as userController from './auth.controller.js'
+import * as userController from './auth.controller.js';
+import { verifyToken } from "../../middleware/authenticate.js";
+import { PERMISSIONS } from "../../config/permission.js";
+import { authorizePermissions } from "../../middleware/authorizePermission.js";
 
 const authRouter = Router();
 
-// Normal User
-authRouter.post("/user/register",userController.register)
+//  User registration
+authRouter.post(
+  "/user/register",
+  verifyToken,
+  authorizePermissions(
+    PERMISSIONS.USER_CREATE,
+    PERMISSIONS.ROLE_ASSIGN
+  ),
+  userController.register
+);
+
+// Manager assign to employee
+authRouter.patch(
+  "/:id/assign-manager",
+  verifyToken,
+  authorizePermissions(PERMISSIONS.EMPLOYEE_UPDATE_ALL),
+  userController.assignManager
+);
+
+
+
+//User login
+
 authRouter.post("/user/login",userController.login)
 
 
