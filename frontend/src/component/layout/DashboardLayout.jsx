4e9d6e92627daa0523ed2e974ad7fa95109632fb
Poonam Vishwakarma <agclub31@gmail.com>
2026-01-06 18:17:57 +0530

    Created Dashboard layout

---
+++ frontend/src/component/layout/DashboardLayout.jsx
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+
+const DashboardLayout = () => {
+  return (
+    <div className="flex h-screen bg-gray-100">
+      <Sidebar />
+
+      <div className="flex flex-col flex-1">
+        <Navbar />
+
+        <main className="flex-1 p-6 overflow-y-auto">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
