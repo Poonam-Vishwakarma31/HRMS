92c0acbc85bd83c757afc3ad6326d619fbf8ab2d
Poonam Vishwakarma <agclub31@gmail.com>
2026-04-16 15:32:36 +0530

    Update CORS configuration to allow specific origins

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,11 @@ dotenv.config();
 const app= express();
 
 app.use(cors({
-  origin: "*"
+  origin: [
+    "http://localhost:5173",
+    "https://hrms-blush-sigma.vercel.app"
+  ],
+  credentials: true
 }));
 
 app.use(express.json());
